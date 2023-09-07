366b43cad68f1dcfe533c9eb46651f4003e6c196
UserBryan <bkk@gmail.com>
2023-09-07 08:44:45 -0700

    Hello puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,19 @@
-#include<stdio.h>
-
-/**
- * main - this is the  main function
- *
- * Return: always 0 (Success)
+/*
+ * File: 4-puts.c
+ * Auth: Bryan-254
  */
 
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
-{ 
-        puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
-}	
+}
