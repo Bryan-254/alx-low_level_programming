90d3292723f9b86a60ef74e94f0abd5e4fce8107
UserBryan <bkk@gmail.com>
2023-09-08 05:54:11 -0700

    All possible combinations of single-digit numbers task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Bryan-254
+ */
+
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
