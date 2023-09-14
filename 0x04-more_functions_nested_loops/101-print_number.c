d40840d1fa0a8aab0c5fa6d0f05e1505de038bd5
UserBryan <bkk@gmail.com>
2023-09-14 08:31:53 -0700

    Function that prints an integer task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - Function that prints an integer
+ * @n: The paramter to be checked
+ *
+ */
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (num > 9)
+	{
+		print_number(num / 10);
+	}
+	_putchar(num % 10 + '0');
+}
